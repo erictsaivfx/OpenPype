f7baf048fb483baabbd35fec7f1f32edd748fdca
erictsaivfx <erictsaivfx@yahoo.com>
2023-10-17 23:17:27 -0700

    add fix 3.16.0 #5238

--- openpype/hosts/substancepainter/plugins/publish/collect_textureset_images.py
+++ openpype/hosts/substancepainter/plugins/publish/collect_textureset_images.py
@@ -1,3 +1,4 @@
+# Update openpype.3.16.0 #5238
 import os
 import copy
 import pyblish.api
@@ -114,7 +115,7 @@ class CollectTextureSet(pyblish.api.InstancePlugin):
         # Clone the instance
         image_instance = context.create_instance(image_subset)
         image_instance[:] = instance[:]
-        image_instance.data.update(copy.deepcopy(instance.data))
+        image_instance.data.update(copy.deepcopy(dict(instance.data)))
         image_instance.data["name"] = image_subset
         image_instance.data["label"] = image_subset
         image_instance.data["subset"] = image_subset
